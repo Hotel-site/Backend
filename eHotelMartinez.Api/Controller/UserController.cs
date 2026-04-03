f7d237514b6fc2c79a30c3201ecc865b335a4e12
Je11yyy <pidlisnyackkiril@gmail.com>
2026-04-03 13:54:12 +0300

    add project and UserController

---
+++ eHotelMartinez.Api/Controller/UserController.cs
@@ -0,0 +1,77 @@
+﻿using eHotelMartinez.Api.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eHotelMartinez.Api.Controller
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //In memory storage for users
+        public static List<User> _users = new();
+        public static int _nextId = 1;
+
+
+        [HttpGet("all")]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound(new { Mesage = $"User with ID {id} Not Found!" });
+            }
+            return Ok(user);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            user.Id = _nextId++;
+            user.CreatedAt = DateTime.UtcNow;
+
+            _users.Add(user);
+
+            return Created($"/api/users/{user.Id}", user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] User updatedUser)
+        {
+            var existUser = _users.FirstOrDefault(u => u.Id == id);
+
+            if (existUser == null)
+            {
+                return NotFound(new { Mesage = $"User with ID {id} Not Found!" });
+            }
+
+            existUser.Username = updatedUser.Username;
+            existUser.Email = updatedUser.Email;
+            return Ok(existUser);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound(new { Mesage = $"User with ID {id} Not Found!" });
+            }
+
+            _users.Remove(user);
+            return NoContent();
+        }
+
+
+    }
+}
