6c62eb0c645cdc6f4c3b6454744cedd20dfba23d
Vent1n <shulyacvalik@gmail.com>
2026-04-19 15:08:31 +0300

    Add DataAccess layer

---
+++ eHotelMartinez.DataAccess/Context/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using eHotelMartinez.Domain.Entities.User;
+
+namespace eHotelMartinez.DataAccess.Context
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<UserData> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbSession.ConnectionString);
+        }
+    }
+}
