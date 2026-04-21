d4caf2d98d3d35619a63a68e46462dd19199539e
Je11yyy <pidlisnyackkiril@gmail.com>
2026-04-21 21:32:43 +0300

    Add for BL UserActions, IUserActions; Deleted Enums/GenderTypes

---
+++ eHotelMartinez.BusinessLogic/Interfacies/IUserActions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eHotelMartinez.Domain.Models.User;
+using eHotelMartinez.Domain.Models.Base;
+
+
+namespace eHotelMartinez.BusinessLogic.Interfacies
+{
+    public interface IUserActions
+    {
+        List<UserDTO> GetAllUsersAction();
+        UserDTO GetUserByIdAction(int id);
+        ResponseMsg ResponceUserCreateAction(UserRegDTO user);
+        ResponseMsg ResponceUserUpdateAction(UserDTO user);
+        ResponseMsg ResponceUserDeleteAction(int id);
+        ResponseMsg ResponceUserLoginAction(UserAuthDTO userAuth);
+    }
+}
