f426cf0cb9e3e68b44e33e2ff6e8448da8a85f11
Vent1n <shulyacvalik@gmail.com>
2026-04-21 16:00:59 +0300

    BusinessLogic Init

---
+++ eHotelMartinez.BusinessLogic/Functions/Products/ProductFlow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eHotelMartinez.BusinessLogic.Interfacies;
+using eHotelMartinez.Domain.Models.Base;
+
+
+namespace eHotelMartinez.BusinessLogic.Functions.Products
+{
+    public class ProductFlow
+    {
+
+    }
+}
