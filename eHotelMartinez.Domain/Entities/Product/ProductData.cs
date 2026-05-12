3c7a4f5b69d457ee0547fa91859b65c0d4bd8a78
Vent1n <shulyacvalik@gmail.com>
2026-05-13 02:23:20 +0300

    Fix Products and Attractions

--- eHotelMartinez.Domain/Entities/Product/ProductData.cs
+++ eHotelMartinez.Domain/Entities/Product/ProductData.cs
@@ -20,10 +20,7 @@ namespace eHotelMartinez.Domain.Entities.Product
         [StringLength(250)]
         public string? Description { get; set; }
 
-        public int CategoryId { get; set; }
-
-        [ForeignKey("CategoryId")]
-        public CategoryData Category { get; set; } 
+        public int? CategoryId { get; set; }
 
         [ForeignKey("CategoryId")]
         public CategoryData Category { get; set; } 
